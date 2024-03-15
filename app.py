b85348e7bedb299d1ecfe91aef055e5b16917422
Jaya Krishna Gaddam <2100090162@kluniversity.in>
2024-03-15 12:31:05 +0530

    Added connection to Gradio Backend

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from flask_pymongo import PyMongo
+from transformers import pipeline
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "1234"
 app.config["MONGO_URI"] = "mongodb+srv://2100090162:manigaddam@deepsheild.kzgpo9p.mongodb.net/textsummarizationsDB"
 mongo = PyMongo(app)
+
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    data = request.form.get('text')
+    summarized_text = summarize_text(data)
+    print(summarized_text)
+    return jsonify(summarized_text=summarized_text)
+
+def summarize_text(text):
+    # Load pre-trained summarization pipeline
+    summarization_pipeline = pipeline("summarization")
+
+    # Generate summary
+    summary = summarization_pipeline(text, max_length=100, min_length=2, do_sample=False)
+
+    # Extract summarized text
+    summarized_text = summary[0]['summary_text']
+    
+    return summarized_text
 
 @app.route('/')
 def index():
@@ -33,6 +54,7 @@ def homepage(name):
 @app.route('/home')
 def home():
     return render_template('home.html')
+
 @app.route('/careers')
 def careers():
     return render_template('careers.html')
@@ -67,6 +89,6 @@ def signup():
 @app.route('/contactus')
 def contactus():
     return render_template('contactus.html')
-    
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
